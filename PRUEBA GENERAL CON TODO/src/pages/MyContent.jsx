058ed45afffac27a4e7688cdd460365bbe35009d
luminagestion <fuocoproduce@gmail.com>
2026-02-18 21:12:29 -0300

    Update MyContent.jsx

--- PRUEBA GENERAL CON TODO/src/pages/MyContent.jsx
+++ PRUEBA GENERAL CON TODO/src/pages/MyContent.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabaseClient";
-import { getSession } from "../lib/session.js";
+import { useAuth } from "../context/AuthContext";
+
 
 const ARTISTS_KEY = "artists";
 
